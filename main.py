2826dfe48c17e33e949553d07e5e9d2233f29716
rayhaantechcare2020 <rayhaantechcare2020@gmail.com>
2023-09-09 14:58:46 +0100

    Final edit

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# creating a api endpoint 2 GET Request Query using slackname and track
+# creating a api endpoint 2 GET Request Query using slack_name and track
 @app.route('/hngtask1', methods=['GET'])
 def get_user_data():
     slack_name = request.args.get('slack_name')
     current_day = datetime.now().strftime("%A")
     track = request.args.get('track')
     utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    github_file_url = ""
-    github_repo_url = ""
+    github_file_url = "https://github.com/salamPhard/hngzuritasks.git"
+    github_repo_url = "https://github.com/salamPhard/hngzuritasks/blob/master/main.py"
     status_code = 200
     data = {
         "slack_name": slack_name,
         "current_day": current_day,
-        "track":track,
+        "track": track,
         "utc_time": utc_time,
-        "github_file_url":github_file_url,
-        "github_repo_url":github_repo_url,
-        "status_code":status_code
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": status_code
     }
     return jsonify(data)
 
